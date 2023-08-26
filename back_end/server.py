17bf51c6086adf565f0930649fc6be7239e40c97
aledrinyth <58565713+aledrinyth@users.noreply.github.com>
2023-08-26 12:06:09 +1000

    Add files via upload

--- back_end/server.py
+++ back_end/server.py
@@ -13,14 +13,26 @@ def flask_hello():
 @app.route('/list', methods=['GET'])
 def get_all_list():
     expenses = database.get_all_list()
+    for expense in expenses:
+        expense['_id'] = str(expense['_id'])
     return jsonify(expenses) 
 
 #to add a expenditure to the database
 
 """ @app.route('/list', methods=['POST'])
-def get_list():
+def add_spending():
     spending = database.
  """
+
+ #to remove expenditure from the database
+
+
+ 
+ #to call and display the different categories
+
+
+
+ #
 
 if __name__ == "__main__":
     app.run(debug=True)
