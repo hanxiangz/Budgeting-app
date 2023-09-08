2764c25e868cf22d8980912bf45f2b2cda086469
hanxiangz <hanxiangz@student.unimelb.edu.au>
2023-09-08 14:15:58 +1000

    updated add transactions form with form validation

--- front_end/src/components/AddTransaction.js
+++ front_end/src/components/AddTransaction.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './AddTransaction.css';
 
+
 function AddTransaction() {
 
   const [formData, setFormData] = useState({
@@ -9,13 +10,41 @@ function AddTransaction() {
     description: '',
   });
 
+  const [amountValidationMessage, setAmountValidationMessage] = useState('');
+  const [categoryValidationMessage, setCategoryValidationMessage] = useState('');
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleSelectChange = (e) => {
+    const selectedCategory = e.target.value;
+    setSelectedOption(selectedCategory);
+    setFormData({ ...formData, category: selectedCategory });
+  };  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
+    // Check if the input value is a valid number using a regular expression
+    if (name === 'amount') {
+      if (/^\d+(\.\d{1,2})?$/.test(value) && parseFloat(value) >= 0) {
+        setAmountValidationMessage('');
+      } else {
+        setAmountValidationMessage('Please enter a valid number');
+      }
+    }
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // if the amount entered is not a number, prevent form submission 
+    if (amountValidationMessage) {
+      return;
+    }
+    // Check if the "category" input is empty
+    if (!formData.category.trim()) {
+      setCategoryValidationMessage('Please select a category');
+      return;
+    }
+
     try {
       const response = await fetch('/add_transaction', {
         method: 'POST',
@@ -32,6 +61,8 @@ function AddTransaction() {
           category: '',
           description: '',
         });
+        setCategoryValidationMessage('');
+        setSelectedOption('');
         alert("You have successfully submitted a transaction")
       } else {
         console.error('Form submission failed');
@@ -41,13 +72,6 @@ function AddTransaction() {
     }
   };
 
-  const [selectedOption, setSelectedOption] = useState('');
-
-  const handleSelectChange = (e) => {
-    const selectedCategory = e.target.value;
-    setSelectedOption(selectedCategory);
-    setFormData({ ...formData, category: selectedCategory });
-  };  
 
   return (
     <div>
@@ -57,9 +81,12 @@ function AddTransaction() {
         <form className="my-form" onSubmit={handleSubmit}>
           <div className="form-group"> 
             <label style={{fontSize: 25}}>
-              Amount:
-              <input style={{ height: '30px', width: '400px', marginLeft: 15, fontSize: 18}} type="text" 
+              Amount($):
+              <input style={{ height: '30px', width: '400px', marginLeft: 15, fontSize: 18}} type="text"
               name="amount" placeholder="Enter amount here" value={formData.amount} onChange={handleChange} />
+              {amountValidationMessage && (
+                <p style={{ color: 'red' }}>{amountValidationMessage}</p>
+              )}
             </label>
           </div>
 
@@ -68,12 +95,19 @@ function AddTransaction() {
               Category:
   
             </label>
-            <select id="dropdown" value={selectedOption} onChange={handleSelectChange}>
+            <select id="dropdown" value={selectedOption} onChange={handleSelectChange} 
+              style={{ height: '30px', width: '400px', marginLeft: 15 }}>
               <option value="">-- Select an option --</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="food">Food</option>
+              <option value="bills">Bills</option>
+              <option value="transport">Transport</option>
+              <option value="healthcare">Healthcare</option>
+              <option value="house">House</option>
+              <option value="savings">Savings</option>
             </select>
+            {categoryValidationMessage && (
+                <p style={{ color: 'red' }}>{categoryValidationMessage}</p>
+            )}
           </div>
 
           <div className="form-group"> 
